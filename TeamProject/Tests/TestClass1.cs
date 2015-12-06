9815c34968364bb9c2219e7b32e47fc5a6a6da27
aska <aska.lee@gmail.com>
2015-12-07 01:30:28 +0800

    v1

---
+++ TeamProject/Tests/TestClass1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject.Tests
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
