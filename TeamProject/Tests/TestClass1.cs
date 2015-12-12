4b31703b3bb89770cd0d6ba729e9dc0adad01f6f
mandyfu <mandyfu84@gmail.com>
2015-12-12 16:06:34 +0800

    Add Borrow Test

--- TeamProject/Tests/TestClass1.cs
+++ TeamProject/Tests/TestClass1.cs
@@ -15,5 +15,22 @@ namespace TeamProject.Tests
         {
             Assert.That(1 + 1, Is.EqualTo(2));
         }
+		[Test]
+		public void Borrow1()
+		{
+			Book book1 = new Book();
+			book1.ISBN = 1;
+			book1.title = "This is a Book";
+			book1.price = 200;
+			book1.location = "Gong Guang Liarary";
+			book1.seller = "Google";
+			book1.status = 1;
+
+			Borrow b = new Borrow();
+			String name = "Mandy";
+			b.BorrowBook(book1, name);
+			Assert.That(b.name, Is.EqualTo("Mandy"));
+			Assert.That(b.id, Is.EqualTo(1));
+		}
     }
 }
