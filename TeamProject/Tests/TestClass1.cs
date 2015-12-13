770be6d6adc89ea9fe0b5217f0aacbb2f34fd086
Ryan Weng <ryan4559@gmail.com>
2015-12-13 19:03:35 +0800

    red light test

--- TeamProject/Tests/TestClass1.cs
+++ TeamProject/Tests/TestClass1.cs
@@ -13,7 +13,7 @@ namespace TeamProject.Tests
         [Test]
         public void Test1()
         {
-            Assert.That(1 + 1, Is.EqualTo(2));
+            Assert.That(1 + 1, Is.EqualTo(0));
         }
 
 		[Test]
@@ -73,7 +73,7 @@ namespace TeamProject.Tests
 		[Test]
 		public void RyanWeng()
 		{
-			Assert.That(40247037,Is.EqualTo(40247037));
+			Assert.That(40247037,Is.EqualTo(402470371));
 
 		}
     }
