82069da035904b942bc738eebffbaac834780b5e
wi1d5ky <poppin.wildsky@gmail.com>
2015-12-21 00:57:44 +0800

    Make some refactor

--- TeamProject/Book.cs
+++ TeamProject/Book.cs
@@ -8,35 +8,38 @@ namespace TeamProject
 {
 	public class Book
 	{
-		private string ISBN_;//1
-		private string title_;//2
-		private string author_;//3
-		private string seller_;//4
-		private string location_;//5
-		private int price_;
-		private bool avalible_ = true;
-		private string borrow_person;
+		// Basic Info
+		private string	ISBN_		= ""; //1
+		private string	title_		= ""; //2
+		private string	author_		= ""; //3
+		private string	seller_		= ""; //4
+		private string	location_	= ""; //5
+		private int	price_		= 0;
+
+		// About Borrow
+		private bool	 avalible_ = true;
+		private string	 borrow_person;
 		private DateTime date_;
 
 		public void SetBookData(string [] d, int p)
 		{
-			ISBN_ = d[0];
-			title_ = d[1];
-			author_ = d[2];
-			seller_ = d[3];
-			location_= d[4];
-			price_  = p;
+			ISBN_		= d[0];
+			title_		= d[1];
+			author_		= d[2];
+			seller_		= d[3];
+			location_	= d[4];
+			price_		= p;
 		}
 
-		public void ChangeStatusToBorrow() { avalible_ = false; }
-		public void SetDate(DateTime d) { date_ = d; }
-		public string GetISBN() { return ISBN_; }
-		public string GetTitle() { return title_; }
-		public string GetAuthor() { return author_; }
-		public string GetSeller() { return seller_; }
-		public string GetLocation() { return location_; }
-		public int GetPrice() { return price_; }
-		public string GetBorrowPerson() { return borrow_person; }
+		public void	ChangeStatusToBorrow() { avalible_ = false; }
+		public void	SetDate(DateTime d) { date_ = d; }
+		public string	GetISBN() { return ISBN_; }
+		public string	GetTitle() { return title_; }
+		public string	GetAuthor() { return author_; }
+		public string	GetSeller() { return seller_; }
+		public string	GetLocation() { return location_; }
+		public int	GetPrice() { return price_; }
+		public string	GetBorrowPerson() { return borrow_person; }
 
 		public void SetISBN(string ISBN) { ISBN_ = ISBN; }
 		public void SetTitle(string Title) { title_ = Title; }
--- TeamProject/Library.cs
+++ TeamProject/Library.cs
@@ -11,27 +11,30 @@ namespace TeamProject
 		public const int maxBookNum = 10000;
 		public int numofbooks = 0;
 		public Book[] books = new Book[maxBookNum];
-		
+
+		// About Search
+		private Book[] findResult = new Book[maxBookNum];
+		private int numOfFind = 0;
+
 		internal void AddBook(Book add)
 		{
 			books[numofbooks] = add;
-			//books[numofbooks].ChangeStatusToBorrow()
 			books[numofbooks].SetDate(DateTime.Now);
 			numofbooks += 1;
 		}
+
 		private int FindBookIndex(String ISBN)
 		{
-			for(int i=0; i<numofbooks; i+=1)
-			{
-				if (books[i].GetISBN() == ISBN) return i;
-			}
+			for(int i = 0 ; i < numofbooks; i += 1)
+				if (books[i].GetISBN() == ISBN)
+					return i;
 			return -1;
 		}
 
 		internal void BorrowBook(String ISBN, String name)
 		{
 			int index = FindBookIndex(ISBN);
-			if(index>=0)
+			if(index >= 0)
 			{
 				books[index].ChangeBorrowPerson(name);
 				books[index].ChangeStatusToBorrow();
@@ -52,32 +55,27 @@ namespace TeamProject
 			}
 			numofbooks -= 1;
 		}
-		public Book[] FindBooks = new Book[maxBookNum];
+
 		public Book[] SearchBookTitle(String title)
 		{
-			int num = 0;
-			for(int i=0;i<numofbooks;i+=1)
-			{
-				if(books[i].GetTitle()==title)
+			for(int i = 0 ; i < numofbooks ; i += 1)
+				if(books[i].GetTitle() == title)
 				{
-					FindBooks[num] = books[i];
-					num += 1;
+					findResult[numOfFind] = books[i];
+					numOfFind += 1;
 				}
-			}
-			return FindBooks;
+			return findResult;
 		}
+
 		public Book[] SearchBookAuthor(String Author)
 		{
-			int num = 0;
-			for(int i=0;i<numofbooks;i+=1)
-			{
-				if(books[i].GetAuthor()==Author)
+			for(int i = 0 ; i < numofbooks ; i += 1)
+				if(books[i].GetAuthor() == Author)
 				{
-					FindBooks[num] = books[i];
-					num += 1;
+					findResult[numOfFind] = books[i];
+					numOfFind += 1;
 				}
-			}
-			return FindBooks;
+			return findResult;
 		}
 	}
 }
